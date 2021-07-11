b0f5271dce04fa702a7b62f9a5470a9f3040a79e
Kimera <hexstudio16@gmail.com>
2021-07-11 12:58:25 +0900

    feat: add base components for MVVM

---
+++ src/Kimera/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kimera.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool?))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool?)value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return !(bool?)value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool?)value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return !(bool)value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Kimera/Validators/NullStringValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Kimera.Validators
+{
+    public class NullStringValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty((string)value))
+            {
+                return new ValidationResult(false, "The name cannot be empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace((string)value))
+            {
+                return new ValidationResult(false, "The name cannot be empty or consist only of spaces.");
+            }
+
+            // If it valid, return true.
+            return new ValidationResult(true, null);
+        }
+    }
+}
