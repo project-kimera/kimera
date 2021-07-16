5b23820b1eeb1252aad4ca0f384581bfe6d14478
June <junimiso04@naver.com>
2021-07-17 00:07:07 +0900

    feat: add settings class

---
+++ src/Kimera/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kimera
+{
+    public class Settings
+    {
+        private static Settings _instance;
+
+        public static Settings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Settings();
+                }
+
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+    }
+}
