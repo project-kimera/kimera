1260d24fd0ba6064b9b330324519b19a4be6a46a
June <junimiso04@naver.com>
2021-08-04 01:23:50 +0900

    feat: add package status indicator.

---
+++ src/Kimera/Converters/PackageStatusToColorConverter.cs
@@ -0,0 +1,43 @@
+﻿using Kimera.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Kimera.Converters
+{
+    [ValueConversion(typeof(PackageStatus), typeof(SolidColorBrush))]
+    public class PackageStatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            PackageStatus status = (PackageStatus)value;
+
+            if (status == PackageStatus.NeedProcessing || status == PackageStatus.Compressed)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 255, 203, 5));
+            }
+            else if (status == PackageStatus.FileNotFound || status == PackageStatus.DataNotFound || status == PackageStatus.Exception)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 217, 30, 24));
+            }
+            else if (status == PackageStatus.Playable)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 0, 230, 64));
+            }
+            else
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Kimera/Converters/PackageStatusToTextConverter.cs
@@ -0,0 +1,54 @@
+﻿using Kimera.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Kimera.Converters
+{
+    [ValueConversion(typeof(PackageStatus), typeof(string))]
+    public class PackageStatusToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            PackageStatus status = (PackageStatus)value;
+
+            if (status == PackageStatus.NeedProcessing)
+            {
+                return "추가 작업 필요";
+            }
+            else if (status == PackageStatus.Compressed)
+            {
+                return "압축 상태";
+            }
+            else if (status == PackageStatus.FileNotFound)
+            {
+                return "파일 미존재";
+            }
+            else if (status == PackageStatus.DataNotFound)
+            {
+                return "데이터 미존재";
+            }
+            else if (status == PackageStatus.Exception)
+            {
+                return "예외 발생";
+            }
+            else if (status == PackageStatus.Playable)
+            {
+                return "실행 가능";
+            }
+            else
+            {
+                return "알 수 없음";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
