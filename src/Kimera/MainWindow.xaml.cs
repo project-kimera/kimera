032f0aaf2bd823a35ab3ca6b0197accb9c41bb78
June <junimiso04@naver.com>
2021-08-05 18:10:02 +0900

    feat: add page transition animation

--- src/Kimera/MainWindow.xaml.cs
+++ src/Kimera/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using Kimera.Pages;
 using Kimera.ViewModels;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media.Animation;
+using System;
 
 namespace Kimera
 {
@@ -17,6 +19,15 @@ namespace Kimera
 
             MainViewModel viewModel = new MainViewModel(ContentFrame);
             this.DataContext = viewModel;
+        }
+
+        private void OnNavigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(300));
+            ContentFrame.BeginAnimation(OpacityProperty, animation);
         }
     }
 }
