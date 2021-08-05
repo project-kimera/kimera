dd6ee6d3264f2941f21a8e39b177692a4920332b
June <junimiso04@naver.com>
2021-08-05 13:20:49 +0900

    feat: add game service base

--- src/Kimera/Services/GameService.cs
+++ src/Kimera/Services/GameService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kimera.Data.Entities;
+using Kimera.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,75 @@ using System.Threading.Tasks;
 
 namespace Kimera.Services
 {
-    class GameService
+    public class GameService
     {
+        #region ::Singleton Members::
+
+        private static GameService _instance;
+
+        public static GameService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameService();
+                }
+
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        #endregion
+
+        public void AddGame(PackageType type, GameMetadata gameMetadata, PackageMetadata packageMetadata, List<Component> components, List<Guid> categories)
+        {
+            if (type == PackageType.Executable)
+            {
+
+            }
+            else if (type == PackageType.Archive)
+            {
+
+            }
+            else if (type == PackageType.Multiple)
+            {
+
+            }
+        }
+
+        public void RemoveGame(Guid gameGuid)
+        {
+
+        }
+
+        public bool ValidateGame(Guid gameGuid)
+        {
+            return false;
+        }
+
+        public void StartGame(Guid gameGuid)
+        {
+
+        }
+
+        public bool ValidateComponents(Guid gameGuid)
+        {
+            return false;
+        }
+
+        public void ProcessComponents(Guid gameGuid)
+        {
+
+        }
+
+        public void RemoveComponents(Guid gameGuid)
+        {
+
+        }
     }
 }
